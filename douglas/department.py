79eb80c97a4535a2e97f12b51159866d0fc292ee
Anita Chang <huilong494@gmail.com>
2020-06-29 01:43:48 -0700

    edit:department

--- douglas/department.py
+++ douglas/department.py
@@ -3,26 +3,26 @@ from .request_utils import bs
 import re
 
 class department:
-    def __init__(self,url):
-        self.url=url
-    def parse(self):
-        req = requests_get(self.url)
-        data = []
-        depsName = ""
-        soup= bs(req.content)
-        #plainText = soup.prettify()
-        depDivs = soup.findAll("a", attrs={'href':re.compile('^\/programs-courses\/faculties')})
-        #depDivss = soup.findAll("span")
-        print(depDivs)
-        course= []
-        for dep in depDivs:
-            depCourse= link.replace("/programs-courses/faculties/","").split("/")
-            depsName = depCourse[0]
-            course.append(dep.text)
-            if depsName == depCourse[0]:
-                course.append(dep.text)
-            else:
-                pass
+    # def __init__(self,url):
+    #     self.url=url
+    # def parse(self):
+    #     req = requests_get(self.url)
+    #     data = []
+    #     depsName = ""
+    #     soup= bs(req.content)
+    #     #plainText = soup.prettify()
+    #     depDivs = soup.findAll("a", attrs={'href':re.compile('^\/programs-courses\/faculties')})
+    #     #depDivss = soup.findAll("span")
+    #     print(depDivs)
+    #     course= []
+    #     for dep in depDivs:
+    #         depCourse= link.replace("/programs-courses/faculties/","").split("/")
+    #         depsName = depCourse[0]
+    #         course.append(dep.text)
+    #         if depsName == depCourse[0]:
+    #             course.append(dep.text)
+    #         else:
+    #             pass
 
 
          #return data
